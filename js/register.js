41d8dbec6fe3cdcd81a18d1cc9dc989ae54871a6
enriquegt <enriquegonzt@gmail.com>
2020-10-16 16:48:23 -0500

    Update register.js

--- js/register.js
+++ js/register.js
@@ -1,21 +1,17 @@
 import User from './user.js'
 import AU from './au.js'
 
-//registro de usuario
+//register user
 document.getElementById('new-user-form').addEventListener('submit', (event) => {
     event.preventDefault();
     const name = document.getElementById('name').value;
-    // const password = document.getElementById('pass').value;
+    const password = document.getElementById('pass').value;
     const email = document.getElementById('email').value;
-    const user = new User(name, email);
-    console.log(user);
+    const user = new User(email, password, name);
     
-    //agregar nuevo usuario
+    //add new user
     const start = new AU();
     start.addUser(user);
     start.resetForm();
-    //agrega el usuario a la base de datos
-    start.addApi(user);
 })
-
 
