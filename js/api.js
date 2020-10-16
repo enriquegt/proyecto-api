248caab244d0ba3b269910d5ef02284c1abc775d
enriquegt <enriquegonzt@gmail.com>
2020-10-16 17:18:52 -0500

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,26 +1,26 @@
 export default class API {
     conexion() {
-        fetch(`https://matter-app.herokuapp.com/api/v1/users`)
+        fetch(`https://matter-app.herokuapp.com/api/v1/skills`)
         .then(response => response.json() ) 
         .then (json => printCards(json)) 
     }
     
 }
 //feedback-recived
-function printCards(emails){
+function printCards(skills){
     const container = document.getElementById('feedback-recived');
     container.innerHTML = '';
-    emails.forEach(email => {
-        const card = returnCardHTML(email);
+    skills.forEach(skill => {
+        const card = returnCardHTML(skill);
         container.innerHTML += card;
     });
 }
 
-function returnCardHTML(q) {
+function returnCardHTML(skill) {
     const card = `<div class="card">
                     <div class="card-body">
-                    <h5 class="card-title">Id - user: ${q.id}</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">${q.email}</h6>           
+                    <h5 class="card-title">Id: ${skill.id}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">${skill.name}</h6>           
                     </div>
                 </div>`
     return card;
