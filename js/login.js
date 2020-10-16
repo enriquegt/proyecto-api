723efa8c06dbba3312e1b53866b9c2fca81613f3
enriquegt <enriquegonzt@gmail.com>
2020-10-16 16:50:45 -0500

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,16 +1,15 @@
 import User from './user.js'
 import Log from './log.js'
-
-//console.log(login);
+//get user and password loggging
 document.getElementById('login-form').addEventListener('submit', (event) => {
     event.preventDefault();
-    const user = document.getElementById('user').value;
     const email = document.getElementById('email').value;
-    // const pass = document.getElementById('pwd').value;
-    const log = new User(user, email);
+    const pass = document.getElementById('pwd').value;
+    const log = new User(email, pass);
     //login 
     const logUser = new Log();
     logUser.loggingUser(log)
     logUser.resetForm();
 })
+
 
