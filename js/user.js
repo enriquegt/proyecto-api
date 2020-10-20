a34965e3828c79d9feb08f5158e46a042ae20301
enriquegt <enriquegonzt@gmail.com>
2020-10-20 00:01:58 -0500

    Update user.js

--- js/user.js
+++ js/user.js
@@ -1,7 +1,20 @@
 export default class User {
     constructor(email, password, name){
         this.name = name
-        this.password = password
         this.email = email
+        this.password = password
+    }
+    searchUser(log){
+        fetch(`https://matter-app.herokuapp.com/api/v1/users`)
+        .then(response => response.json() ) 
+        .then (json => searchUser(json))
+
+        //search if the email is not used in the API
+        function searchUser(users){
+        const search = users.find(user=>user.email === log.email)
+        name = search.name;
+        return name
+        }
+        return name
     }
 }
