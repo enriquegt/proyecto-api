5a76be718df83e5597cd50028a46ac7ece813b38
neto-es <ernesto867es@gmail.com>
2020-10-20 00:13:09 -0500

    registro arreglado

--- js/register.js
+++ js/register.js
@@ -6,7 +6,8 @@ document.getElementById('new-user-form').addEventListener('submit', (event) => {
     event.preventDefault();
     const password = document.getElementById('pass').value;
     const email = document.getElementById('email').value;
-    const user = new User(email, password);
+    const name = document.getElementById('name').value;
+    const user = new User(email, password, name);
     
     //add new user
     const start = new AU();
